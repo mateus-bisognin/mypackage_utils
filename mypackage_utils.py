61f4a5041f09115ef446ce71480bfff466011f99
Mateus Michelon Bisognin <mateus_bisognin@hotmail.com>
2021-12-27 15:53:27 -0300

    inicio dia 27

--- mypackage_utils.py
+++ mypackage_utils.py
@@ -1,5 +1,6 @@
-import bpy
-import bmesh
+# dependencies
+# import bpy
+# import bmesh
 
 def backLeftBottomVertex(listOfVerts):
 	s = sorted(listOfVerts, key = lambda vert: (vert.co[0], vert.co[1], vert.co[2]))
@@ -10,29 +11,29 @@ def backLeftBottomVertex(listOfVerts):
 def bmBackLeftBottomVertex(bm):
 	bm.verts.ensure_lookup_table()
 	verts = list(bm.verts)
-	vertex = Utilities.backLeftBottomVertex(listOfVerts)
-		if not Utilities.isFirstVertexOnEveryAxis(bm, vertex):
-			raise ValueError('Error:  has no unique vertex simultaneously at furthest back, left and bottom sides')
-		return vertex
+	vertex = backLeftBottomVertex(listOfVerts)
+	if not isFirstVertexOnEveryAxis(bm, vertex):
+		raise ValueError('Error:  has no unique vertex simultaneously at furthest back, left and bottom sides')
+	return vertex
 
 	
 def isFirstVertexOnAxis(bm, vertex, axis):
 	bm.verts.ensure_lookup_table()
 	verts = list(bm.verts)
 	#axis x = 0, y = 1, z = 2
-		s = sorted(verts, key = lambda vert: (vert.co[axis]))
-		return s[0].co[axis] == vertex.co[axis]
+	s = sorted(verts, key = lambda vert: (vert.co[axis]))
+	return s[0].co[axis] == vertex.co[axis]
 
 	
 def isFirstVertexOnEveryAxis(bm, vertex):
-	return all(Utilities.isFirstVertexOnAxis(bm, vertex, axis) for axis in range(0,3))
+	return all(isFirstVertexOnAxis(bm, vertex, axis) for axis in range(0,3))
 	
 # p = bpy.data.objects[1]
 # bm = bmesh.new()
 # bm.from_mesh(p.data)
 
 # print(p)
-# v = Utilities.backLeftBottomVertex(bm)
+# v = backLeftBottomVertex(bm)
 # print(v)
 
 # print(v.co)
