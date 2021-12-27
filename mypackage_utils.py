03d8aed648b96a29f30292b2b304a6118c9befb3
Mateus Michelon Bisognin <mateus_bisognin@hotmail.com>
2021-12-27 17:10:31 -0300

    adicionar funcoes

--- mypackage_utils.py
+++ mypackage_utils.py
@@ -6,7 +6,6 @@ def backLeftBottomVertex(listOfVerts):
 	s = sorted(listOfVerts, key = lambda vert: (vert.co[0], vert.co[1], vert.co[2]))
 	vertex = s[0]
 	return vertex
-
 	
 def bmBackLeftBottomVertex(bm):
 	bm.verts.ensure_lookup_table()
@@ -15,7 +14,6 @@ def bmBackLeftBottomVertex(bm):
 	if not isFirstVertexOnEveryAxis(bm, vertex):
 		raise ValueError('Error:  has no unique vertex simultaneously at furthest back, left and bottom sides')
 	return vertex
-
 	
 def isFirstVertexOnAxis(bm, vertex, axis):
 	bm.verts.ensure_lookup_table()
@@ -24,10 +22,26 @@ def isFirstVertexOnAxis(bm, vertex, axis):
 	s = sorted(verts, key = lambda vert: (vert.co[axis]))
 	return s[0].co[axis] == vertex.co[axis]
 
-	
 def isFirstVertexOnEveryAxis(bm, vertex):
 	return all(isFirstVertexOnAxis(bm, vertex, axis) for axis in range(0,3))
-	
+
+def rotateMesh(bm, matrix):
+	bmesh.rotate(bm, verts = bm.verts, matrix = matrix)
+
+
+def get_vertices_in_group(a,index):
+    # a is a Blender object
+    # index is an integer representing the index of the vertex group in a
+    vlist = []
+    for v in a.data.vertices:
+        for g in v.groups:
+            if g.group == index:
+                vlist.append(v.index)
+    return vlist
+
+
+
+
 # p = bpy.data.objects[1]
 # bm = bmesh.new()
 # bm.from_mesh(p.data)
