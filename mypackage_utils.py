c29c7a8c3454e27bcb80c324701c6ab4ad7fef41
Mateus Michelon Bisognin <mateus_bisognin@hotmail.com>
2021-12-28 17:54:39 -0300

    testes

--- mypackage_utils.py
+++ mypackage_utils.py
@@ -1,6 +1,13 @@
 # dependencies
 # import bpy
-# import bmesh
+import bmesh
+
+
+# dev environment para o modulo
+# from importlib import reload  # Python 3.4+
+# from mypackage_utils import mypackage_utils as u
+# reload(u)
+
 
 def backLeftBottomVertex(listOfVerts):
 	s = sorted(listOfVerts, key = lambda vert: (vert.co[0], vert.co[1], vert.co[2]))
@@ -40,7 +47,54 @@ def get_vertices_in_group(a,index):
     return vlist
 
 
+def duplicate_object_unlinked(obj):
+	collection = obj.users_collection[0]
+	newCopy = obj.copy()
+	newCopy.data = obj.data.copy()
+	collection.objects.link(newCopy)
+	return newCopy
 
+def duplicate_object_linked(obj):
+	collection = obj.users_collection[0]
+	newCopy = obj.copy()
+	collection.objects.link(newCopy)
+	return newCopy
+
+def plug_object_to_socket(plug, face):
+	socketNormal = face.normal
+
+def place_copy_of_obj2_to_obj1_mesh_vertex(obj1, obj2, vertex, merge = True):
+	# obj2 origin will to vertex
+	bm1 = bmesh.new()
+	bm1.from_mesh(obj1.data)
+	bmesh.ops.translate(bm1, verts = bm1.verts, vec = -vertex.co)
+	bm1.from_mesh(obj2.data)
+	bmesh.ops.translate(bm1, verts = bm1.verts, vec = vertex.co)
+	if merge:
+		bmesh.ops.remove_doubles(bm1, verts = bm1.verts, dist = 0.000001)
+	bm1.to_mesh(obj1.data)
+	bm1.free()
+
+
+def get_faces_from_facemap_index(bm, facemap_index):
+	fm = bm.faces.layers.face_map.verify()
+	faces = []
+	for face in bm.faces:
+		face_idx = face.index
+		map_idx = face[fm]
+		if map_idx == facemap_index:
+			faces.append(face)
+	return faces
+
+def get_normal_from_first_face_in_facemap(obj, facemap_name):
+	bm = bmesh.new()
+	bm.from_mesh(obj.data)
+	facemap_index = obj.face_maps[facemap_name].index
+	faces = get_faces_from_facemap_index(bm, facemap_index)
+	first_face = faces[0]
+	normal = first_face.normal
+	bm.free()
+	return normal
 
 # p = bpy.data.objects[1]
 # bm = bmesh.new()
